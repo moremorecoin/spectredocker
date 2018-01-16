21e0f082384a86b2532a842b231ebcda1913d069
More Coin <moremorecoin@gmail.com>
2018-01-15 19:43:52 -0800

    v1.0

---
+++ image/source/staking.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+$docmd="/bin/spectrecoindi -conf=/root/configure.conf";
+print "Enter The Password\n";
+system ( "stty -echo");
+my $password=<STDIN>;
+chomp $password;
+system ( "stty echo");
+print "Trying to unlock the wallet for staking\n";
+system ("$docmd walletlock");
+system ("$docmd walletpassphrase '$password' 315360000 true"); # ten years
+sleep(10);
+system ("$docmd getstakinginfo");
